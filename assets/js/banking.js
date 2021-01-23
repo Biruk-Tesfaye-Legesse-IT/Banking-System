8ee78ef332457c4bbecd5598a65463473b5945b1
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2021-01-23 23:49:42 +0300

    Transfer function added.

--- assets/js/banking.js
+++ assets/js/banking.js
@@ -77,3 +77,33 @@ function checkBalance(balance) {
 }
 
 // ==============================================================================================================
+
+function transfer(balance) {
+    if (balance <= 50) {
+        console.log("We are sorry your current balance is insufficient for a withdrawal");
+
+    } else {
+        const amount = prompt("Enter amount you want to transfer: ");
+        if (amount > balance - 50) {
+            console.log("We are sorry your current balance is insufficient for a withdrawal of such amount");
+        } else {
+            const account = prompt("Enter the account number you want to transfer to: ");
+            balance = balance - amount;
+            let confirm = prompt("Press 1 to confirm or 0 to correct the transaction \n  Recipient: " + account + "\n Amount: " + amount);
+            switch (confirm) {
+                case "1":
+                    console.log("You have transferred " + amount + " ETB to " + account + "\n Your current balance is: " + balance + " ETB");
+                    break;
+
+                case "0":
+                    transfer();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    }
+
+}
