9c5b3d514076924ba1272a9a36c8ce7ea580154a
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2021-01-24 09:34:54 +0300

    Banking system with objects started and objects created.

---
+++ assets/js/banking(with objects).js
@@ -0,0 +1,28 @@
+// jshint esversion:6
+
+
+let person1 = {
+    Name: "",
+    Password: "",
+    Account: "",
+    Balance: ""
+
+
+
+};
+
+let person2 = {
+    Name: "",
+    Password: "",
+    Account: "",
+    Balance: ""
+
+};
+
+let person3 = {
+    Name: "",
+    Password: "",
+    Account: "",
+    Balance: ""
+
+};
